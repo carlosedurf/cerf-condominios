0ac2ae0bbd7865faeab43bfe5bc24f7eb1d3b9da
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2023-01-18 20:09:54 -0300

    add file api

---
+++ src/services/api.tsx
@@ -0,0 +1,41 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const baseUrl = 'https://api.b7web.com.br/devcond/api';
+
+const request = async (method: string, endpoint: string, params: any, token?: string|null) => {
+    method = method.toLowerCase();
+    let fullUrl = `${baseUrl}${endpoint}`;
+    let body: any = {};
+
+    switch(method) {
+        case 'get':
+                const queryString = new URLSearchParams(params).toString();
+                fullUrl += `${queryString}`;
+            break;
+        case 'post':
+        case 'put':
+        case 'delete':
+                body = JSON.stringify(params);
+            break;
+    }
+
+    let headers: any = {'Content-Type': 'application/json'};
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    let req = await fetch(fullUrl, {method, headers, body});
+    const res = await req.json();
+    return res;
+};
+
+export default {
+    getToken: async () => {
+        return await AsyncStorage.getItem('token');
+    },
+    validateToken: async () => {
+        const token: string|null = await AsyncStorage.getItem('token');
+        const json = await request('post', '/auth/validate', {}, token);
+        return json;
+    },
+};;
