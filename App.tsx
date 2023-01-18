f71b02e9847e8634a601a0a469e0b58d063e466d
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2023-01-18 07:56:01 -0300

    add context and reducer

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {View, Text} from "react-native";
+import {StateProvider} from "./src/contexts/StateContext";
 
 export default () => {
   return (
-    <View>
-      <Text>Hello, World!</Text>
-    </View>
+    <StateProvider>
+      <View>
+        <Text>Olá mundo!</Text>
+      </View>
+    </StateProvider>
   );
 };
